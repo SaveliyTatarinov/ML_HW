ac544f514bbf4a96ca012ffd1b784a59d07de415
Saveliy1 <saveliy@example.com>
2024-10-24 21:33:03 +0500

    lab2

---
+++ ml_hw_2/debug_example.py
@@ -0,0 +1,11 @@
+def calculate_sum(a: int, b: int) -> int:
+    return a + b
+
+
+def main():
+    result = calculate_sum(5, 2)
+    print(f"Сумма: {result}")
+
+
+if __name__ == "__main__":
+    main()
